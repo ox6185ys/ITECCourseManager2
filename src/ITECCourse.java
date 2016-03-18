abc688388ad6e803e80ca0ee653a378e6b25b2c9
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-03-18 13:46:16 -0500

    Update ITECCourseManager

---
+++ src/ITECCourse.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+/**
+ * Created by ox6185ys on 3/18/2016.
+ */
+public class ITECCourse {//Set private elements for ITECCourse Class.
+    private String name;
+    private int code;
+    private ArrayList<String> students;
+    private int maxStudents;
+    private String roomNumber;
+    //Constructorfor ITECCourse.
+    ITECCourse(String courseName, int courseCode, int courseMaxStudents, String roomNumber)
+    {
+        this.name = courseName;
+        this.code = courseCode;
+        this.students = new ArrayList<String>();
+        this.maxStudents = courseMaxStudents;
+        this.roomNumber = roomNumber;
+    }
+    public void addStudent(String studentName)
+    {
+        //don't need to check if students ArrayList is null –
+        //we know that the constructor set it up -- Clara Comments.
+        students.add(studentName);
+    }
+
+    public int availableSeats()//Method for course availability in ITECCourseManager Class
+    {
+        return (maxStudents - students.size());
+    }
+    public void writeCourseInfo() {//Display Course Details.
+        System.out.println("Course Name: " + this.name);
+        System.out.println("Course Code: " + this.code);
+        System.out.println("Students enrolled:");
+        for (String student : this.students) {
+            System.out.println(student);
+        }
+        //System.out.println("There are " + getNumberOfStudents() + " students enrolled");
+        System.out.println("The class meets in room number " + roomNumber );
+        System.out.println("The max number of students that can enroll in this course is " + this.maxStudents +'\n');
+    }
+    //Return the total to ITECCourse Class.
+    public String getName()
+    {
+        return name;
+    }
+}
---
+++ src/ITECCourseManager.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+/**
+ * Created by ox6185ys on 3/18/2016.
+ */
+public class ITECCourseManager {
+    public static void main(String args[]) {
+        //ITECCourse Constructor for 3 courses.
+        ITECCourse maintenanceCourse = new ITECCourse("Microcomputer Systems Maintenance", 1310, 20, "50");
+
+        maintenanceCourse.addStudent("Anna");
+        maintenanceCourse.addStudent("Bill");
+        maintenanceCourse.addStudent("Carl");
+        maintenanceCourse.writeCourseInfo();
+
+        ITECCourse datacomCourse = new ITECCourse("Data Communications", 1425, 30, "60");
+
+        datacomCourse.addStudent("Dave");
+        datacomCourse.addStudent("Ed");
+        datacomCourse.addStudent("Flora");
+        datacomCourse.writeCourseInfo();
+
+        ITECCourse infoTechConceptsCourse = new ITECCourse("Info Tech Concepts", 1100, 30, "T3050");
+        infoTechConceptsCourse.addStudent("Max");
+        infoTechConceptsCourse.addStudent("Nancy");
+        infoTechConceptsCourse.addStudent("Orson");
+        infoTechConceptsCourse.writeCourseInfo();
+        //Assemble ArrayList to get availability.
+        ArrayList <ITECCourse> allCourses = new ArrayList<ITECCourse>();
+        allCourses.add(maintenanceCourse);
+        allCourses.add(datacomCourse);
+        allCourses.add(infoTechConceptsCourse);
+
+        for (ITECCourse course : allCourses)
+        {
+            System.out.println(course.getName());
+            System.out.println("There are " + course.availableSeats() + " spaces open in this class.\n");
+        }
+    }
+}
